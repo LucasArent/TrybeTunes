b2d8ad5621b64487c92f62375c3bb1466c5085f1
LucasArent <arentlucas2708@gmail.com>
2023-03-21 07:42:23 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import React from 'react';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <p>
+        {' '}
+        TrybeTunes
+        <BrowserRouter>
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/search" component={ Search } />
+        </BrowserRouter>
+      </p>
+    );
   }
 }
 
 export default App;
-
