4e2acff92b2c1ed139061c748edc69c4e88cdd74
LucasArent <arentlucas2708@gmail.com>
2023-04-01 15:39:59 -0300

    requisito 5, .length não tava funcionando, escrevi novamente e passou

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    tileArtistic: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      tileArtistic: event.target.value,
+    });
+  };
+
   render() {
+    const { tileArtistic } = this.state;
+    const DisabeButton = tileArtistic.length < 2; // verificar se o input tem 2 ou mais caracteres
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist-input">
+            pesquisar
+          </label>
+          <input
+            type="text"
+            id="search-artist-input"
+            name="search-artist-input"
+            data-testid="search-artist-input"
+            value={ tileArtistic }
+            onChange={ this.handleChange }
+          />
+          <button
+            id="findArt"
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ DisabeButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
