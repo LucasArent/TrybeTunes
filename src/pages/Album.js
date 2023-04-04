87c16810c851602fd990f63ac45327ceb7c12f73
LucasArent <arentlucas2708@gmail.com>
2023-04-04 16:11:16 -0300

    requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import musicsAPI from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    openMusic: [],
+    album: '',
+    artistName: '',
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const openMusic = await musicsAPI(id);
+
+    const [benningMusic, ...otherMusic] = openMusic;
+
+    this.setState({
+      openMusic: [...otherMusic],
+      album: benningMusic.collectionName,
+      artistName: benningMusic.artistName,
+    });
+  }
+
+  listMusicas = (openmusic) => openmusic.map((getmusic, home) => (
+    <li key={ `${getmusic.tracks}-${home}` }>
+      <p>{getmusic.trackName}</p>
+      <audio controls data-testid="audio-component">
+        Audio
+      </audio>
+    </li>
+  ));
+
   render() {
+    const { openMusic,
+      album,
+      artistName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <p data-testid="artist-name">{artistName}</p>
+        <p data-testid="album-name">{album}</p>
+        <li>{this.listMusicas(openMusic)}</li>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     tileArtistic: '',
+    isLoading: false,
+    albums: [],
+    error: null,
   };
 
   handleChange = (event) => {
@@ -12,17 +17,64 @@ class Search extends Component {
     });
   };
 
-  render() {
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
     const { tileArtistic } = this.state;
-    const DisabeButton = tileArtistic.length < 2; // verificar se o input tem 2 ou mais caracteres
+
+    try {
+      this.setState({ isLoading: true });
+
+      const albums = await searchAlbumsAPI(tileArtistic);
+
+      this.setState({
+        albums,
+        isLoading: false,
+        error: null,
+      });
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        error: error.message,
+      });
+    }
+  };
+
+  renderAlbums = () => {
+    const { albums } = this.state;
+
+    if (albums.length === 0) {
+      return <p>Nenhum álbum foi encontrado</p>;
+    }
+
+    return (
+      <div>
+        {albums.map((album) => (
+          <div key={ album.collectionId }>
+            <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+            <h2>{album.collectionName}</h2>
+            <p>{album.artistName}</p>
+            <Link
+              to={ `/album/${album.collectionId}` }
+              data-testid={ `link-to-album-${album.collectionId}` }
+            >
+              Ver detalhes
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  render() {
+    const { tileArtistic, isLoading, error } = this.state;
+    const disabledButton = tileArtistic.length < 2; // verificar se tem 2 ou mais caracteres
 
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="search-artist-input">
-            pesquisar
-          </label>
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="search-artist-input">Pesquisar</label>
           <input
             type="text"
             id="search-artist-input"
@@ -35,11 +87,28 @@ class Search extends Component {
             id="findArt"
             type="submit"
             data-testid="search-artist-button"
-            disabled={ DisabeButton }
+            disabled={ disabledButton }
           >
             Pesquisar
           </button>
         </form>
+
+        {isLoading && <p>Carregando...</p>}
+
+        {error && <p>Ocorreu um erro ao buscar os álbuns. Por favor, tente novamente.</p>}
+
+        {!isLoading && !error && (
+          <div>
+            {tileArtistic && (
+              <p>
+                Resultado de álbuns de:
+                {' '}
+                {tileArtistic}
+              </p>
+            )}
+            {this.renderAlbums()}
+          </div>
+        )}
       </div>
     );
   }
