284e2fac8ec093a36cee096b21586be1ad4c0953
LucasArent <arentlucas2708@gmail.com>
2023-04-06 04:05:55 -0300

    requisito 9 talvez

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
   state = {
     checked: false,
+    favoritePlaylist: [],
     load: false,
   };
+
+  async componentDidMount() {
+    this.setState({ load: true });
+    const favoritePlaylist = await getFavoriteSongs();
+    this.setState({ load: false, favoritePlaylist }, () => {
+      this.fav();
+    });
+  }
 
   async newInput({ target }) { // usando componentDidMount simplesmente não via se existia um checkbox para cada musica
     const { audio } = this.props;
     const verify = target.checked;
-    this.setState({
-      checked: verify,
-      load: true,
-    });
-    await addSong(audio); // import da api
-    this.setState({
-      load: false,
-    });
+    this.setState({ load: true });
+    if (verify) {
+      await addSong(audio); // import da api
+    } this.setState({
+      checked: verify, load: false });
+  }
+
+  fav() {
+    const { favoritePlaylist } = this.state;
+    const { trackId } = this.props;
+    const favorite = favoritePlaylist.some(
+      (favAlbum) => favAlbum.trackId === trackId,
+    );
+    this.setState({ checked: favorite });
   }
 
   render() {
@@ -30,7 +45,7 @@ class MusicCard extends Component {
           <p><strong>Carregando...</strong></p>
         ) : (
           <div>
-            <p id="find-track-component">{trackName}</p>
+            <p>{ trackName }</p>
             <audio data-testid="audio-component" controls key={ trackId }>
               <track kind="captions" />
               O seu navegador não suporta o elemento
